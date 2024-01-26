3f6dfc04504287ce942e1f901a84ec47fa010ed1
uriel-martinez1 <martinezu444@gmail.com>
2024-01-25 20:20:29 -0500

    Adding home view

---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div v-if="isLoading">
+        <div>
+            <Loading></Loading>
+        </div>
+    </div>
+    <div v-else>
+        <AppHeader></AppHeader>
+        <JokeResult></JokeResult>
+    </div>
+</template>
+
+<script>
+import AppHeader from '../components/AppHeader.vue'
+import JokeResult from '../components/JokeResult.vue'
+import Loading from '../components/LoadingView.vue'
+
+export default {
+    components: {
+        AppHeader,
+        JokeResult,
+        Loading
+    },
+}
+</script>
+
+<style></style>
